8f29bd4f7e9929999fc3dce15f60f645a9e3fff4
sabarishakash <sabarish16797@gmail.com>
2024-02-06 13:57:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,311 @@
-import logo from './logo.svg';
-import './App.css';
+import img from "./Images/MFC LOGO.jpeg"
+import img1 from "../src/Images/gradient-football-field-background_23-2149013355.avif"
 
+import img2 from "../src/Images/download.jpg"
+import img3 from "../src/Images/FC-Madras-ALC-736x492.jpg"
+import img4 from "../src/Images/FCM-hero-Image-1-1536x680.png"
+import img5 from "../src/Images/download (1).jpg"
+import img6 from "../src/Images/download (2).jpg"
+import img7 from "../src/Images/download (3).jpg"
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { MdOutlineStar } from "react-icons/md";
+import { SiLens } from "react-icons/si";
+import { GiThink } from "react-icons/gi";
+import { FaPersonRunning } from "react-icons/fa6";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark fixed-top">
+      <div className="container">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span></button>
+          <img src={img} className="img-fluid navbar-brand" width="70px"/>
+
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+
+<ul className="navbar-nav ps-5">
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>Home</a></li>
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>Product</a></li>
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>About</a></li>
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>Specification</a></li>
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>Policy</a></li>
+  <li className="nav-item ps-5"><a className="nav-link text-white" href="#" style={{fontSize:"20px"}}>Contactus</a></li>
+
+
+
+</ul>
+
+</div>
+      </div>
+     
+
+    </nav>
+    <div className="container-fluid ny">
+      <div className="row  jk">
+        <div className="col-lg-2 ps-5 mt-4" style={{fontSize:"12px",fontWeight:"bold"}}>
+          <p>Since:1980</p>
+        </div>
+        <div className="col-lg-8">
+          &nbsp;
+
+        </div>
+        <div className="col-lg-2 mt-4" style={{fontSize:"12px",fontWeight:"bold"}}>
+          <p>Phone : 04256-231153</p>
+          <p>Mobile: 98431 24255</p>
+          <p>Mail ID:moovendarfc@gmail.com</p>
+
+        </div>
+
+
+      </div>
+      <div className="row jk">
+        <div className="col-lg-2">
+
+        </div>
+        <div className="col-lg-2">
+          <img className="img-fluid"  src={img}/>
+
+        </div>
+        <div className="col-lg-6 text-center mt-4">
+          <h1 className="bit">MOOVENDAR FOOTBALL CLUB</h1>
+          <p style={{fontWeight:"bold"}}>(Affliated by Tamilnadu Football Association)</p>
+          <p style={{fontWeight:"bold"}}>58/A.Palanipuram 4th Street,BHAVANI-638301.Erode District.</p>
+
+
+
+
+        </div>
+
+      </div>
+      <div className="row sim40 align-items-center">
+        <div className="col-lg-12 text-center">
+          
+         
+
+        <h1 className="ve mb-2">Football is a game of inches<br/> played on a field of yards.</h1>
+        <p className="gt">The beautiful thing about football <br/>is that anyone can become a hero in 90 minutes.</p>
+       <button className="btn btn-outline-warning p-2 gi mt-2 rounded-5">Read More</button>
+        </div>
+
+        
+
+      </div>
+      <div className="row mt-5 justify-content-center gap-4">
+        <div className="col-lg-2 p-5  text-center shadow ji">
+      
+<img src={img2} width="50px" height="50px" className="img-fluid mb-4"/>
+
+
+<p>Years Experience</p>
+
+
+<h1>50</h1>
+
+
+        </div>
+        <div className="col-lg-2 p-5  text-center shadow ji">
+      
+      <img src={img5} width="50px" height="50px" className="img-fluid mb-4"/>
+      
+      
+      <p>Skilled Professionals</p>
+      
+      
+      <h1>175</h1>
+      
+      
+              </div>
+              <div className="col-lg-2 p-5  text-center shadow ji">
+      
+      <img src={img6} width="50px" height="50px" className="img-fluid mb-4"/>
+      
+      
+      <p>Total Products</p>
+      
+      
+      <h1>135</h1>
+      
+      
+              </div>
+              <div className="col-lg-2 p-5  text-center shadow ji">
+      
+      <img src={img7} width="50px" height="50px" className="img-fluid mb-4"/>
+      
+      
+      <p>Order Everyday</p>
+      
+      
+      <h1>9357</h1>
+      
+      
+              </div>
+                  
+      </div>
+
+      <div className="row">
+        <div className="col-lg-12 text-center mt-5">
+          <h1 style={{color: "#2c438a"}}>Academy For Youth Excellence</h1>
+
+        </div>
+
+
+      </div>
+      <div className="row mt-5 text-center">
+        
+
+        <div className="col-lg-3">
+        <MdOutlineStar style={{color: "#2c438a", fontSize:"90px"}} />
+        <p className=" mt-4">AIFF 3-Star rated Football<br/> Academy in India offering<br/> scholarships & financial<br/> assistance for residential & non-<br/>residential student-athlete<br/> programs for the development of <br/>elite youth footballers under-13 &<br/> under-15 groups.</p>
+
+        </div>
+        
+        <div className="col-lg-3">
+        <SiLens  style={{color: "#2c438a", fontSize:"90px"}} />
+        <p className="mt-4">Built based on prescribed UEFA<br/> standards for youth academies,<br/> FC Madras provides student-<br/>athletes with world-class facilities<br/> for football training,<br/> entertainment, and education for<br/> overall youth development.</p>
+
+        </div>
+       
+        <div className="col-lg-3">
+        <GiThink style={{color: "#2c438a", fontSize:"90px"}} />
+        <p className="mt-4">Athletes are coached on strength <br/> & conditioning, mental well-<br/>being, and nutrition by<br/> specialised practitioners. We <br/>believe quality football & <br/>education is paramount to the<br/> success of the academy.</p>
+
+        </div>
+       
+        <div className="col-lg-3">
+        <FaPersonRunning style={{color: "#2c438a", fontSize:"90px"}} />
+        <p className="mt-4">Cutting-edge sports science and <br/>technology to continuously <br/>monitor and analyse the progress<br/> of our athletes. Real-time video<br/> analysis of coaching, and playing <br/>sessions through trackers.</p>
+        </div>
+      </div>
+      <div id="demo" class=" container carousel slide" data-bs-ride="carousel">
+
+{/* Indicators/dots */}
+<div className="carousel-indicators">
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" className="active"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+</div>
+
+{/* The slideshow/carousel */}
+<div className="carousel-inner p-5">
+  <div className="carousel-item active text-center" style={{backgroundColor:"#2c438a",color:"white"}}>
+   <h1 className=" mt-4">Testimonials</h1>
+   <h4 className="p-4">FC vision is unique and probably the only academy in the country<br/>focussed on grassroots training programs
+   for student-athletes. I had the chance <br/>to watch a couple of youth games in the U-15 and U-13 categories,and as a<br/>
+   national coach,I can vouch for their training and coaching techniques.The<br/>coaching staff has designed a well-
+   structured training program inclusive of<br/>game time,strength & conditioning, and entertainment for the players.I have<br/>
+   immense faith in the management of FC and their commitment to <br/>football in India.  </h4>
+  </div>
+  <div className="carousel-item text-center" style={{backgroundColor:"#2c438a",color:"white"}}>
+  <h1 className=" mt-4">Testimonials</h1>
+  <h4 className="p-4">The cleanliness and tidiness inside the ground and premises definitely prove to <br/>
+  be a good resource for the talented and young players who want to train in a <br/>High-Performance Football Academy.
+  I as a parent feel FC is the <br/>greatest hope for developing football players who want to take football for their<br/>
+  professional development. Playing and training for 11 a side matches will<br/>definitely help these youth I-league players
+  improve their endurance and vision<br/>for a 90-minute match.   </h4>
+  </div>
+  <div className="carousel-item text-center" style={{backgroundColor:"#2c438a",color:"white"}}>
+  <h1 className=" mt-4">Testimonials</h1>
+  <h4 className="p-4">Having witnessed the myraid bumpy footballing surfaces around the city with<br/>serious
+  space limitations,FC arena is a dream come true for footballers.<br/>Coaching (and managing) is incredibly important
+  for kids in their early-mid<br/>teens and it is here that I've seen the biggest change in the last few months.I<br/>
+  love when my son is eager to talk about how his training session went and that<br/> is pretty much every day now.
+  The academy coaches for sportsmanship,beyond the game and this has a positive impact on children outside the 
+  football ground <br/>and I can already see signs of it with my son.   </h4>
+  </div>
+</div>
+
+{/* Left and right controls/icons */}
+<button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+  <span className="carousel-control-prev-icon"></span>
+</button>
+<button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+  <span className="carousel-control-next-icon"></span>
+</button>
+</div> 
+           <div className="row">
+            <div className="col-lg-12 text-center">
+            <h1 style={{color: "#2c438a"}}>360° Athlete Wellness & Training</h1> 
+</div>
+             </div>       
+
+             <div className="row justify-content-around">
+             <div className="col-lg-4">
+             <img src={img3} className="img-fluid"/>
+            <h5 className="mt-3" style={{color: "#2c438a"}}>Sports Integrated Education</h5>
+            <p style={{fontSize:"20px"}}>Most academic institutions have a clear <br/>pathway when it comes to academics and <br/>sports is part of that curriculum . FC Madras<br/> academy curriculum focuses on sports <br/>integrated education which encompasses…</p>
+            <h5 style={{textDecoration:"underline",color:"blue"}}>Read more</h5>
+             </div>
+             <div className="col-lg-4">
+             <img src={img3} className="img-fluid"/>
+            <h5 className="mt-3" style={{color: "#2c438a"}}>Holistic Athlete Development</h5>
+            <p style={{fontSize:"20px"}}>FC Madras athlete pathway model is not only<br/> to develop football players but their character<br/> as well. Talent development is development<br/> as a human being and not just as a football<br/> player…</p>
+            <h5 style={{textDecoration:"underline",color:"blue"}}>Read more</h5>
+             </div>
+             <div className="col-lg-4">
+             <img src={img3} className="img-fluid"/>
+           <h5 className="mt-3" style={{color: "#2c438a"}}>Sports Nutrition</h5>
+           <p style={{fontSize:"20px"}}>We are developing expertise in sports<br/> nutrition and exercise performance along <br/>with partners to deliver the best sports<br/> nutrition program designed for youth <br/>athletes…</p>
+           <h5 style={{textDecoration:"underline",color:"blue"}}>Read more</h5>
+             </div>
+             </div>  
+             
+             <div className="row">
+              <div className="col-lg-12">
+                <img src={img4} className="img-fluid"/>
+
+              </div>
+              </div>                          
+<div className="row text-white justify-content-end" style={{backgroundColor:"#2c438a",lineHeight:"30px"}} >
+  <div className="col-lg-2">
+    <img src={img} className="img-fluid mt-4" width="100px" height="100px"/>
+
+  </div>
+  <div className="col-lg-3 mt-3">
+    <ul style={{listStyle:"none"}}>
+
+      <li>Home</li>
+      <li>About us</li>
+      <li>Life@Academy</li>
+      <li>Academy Programs</li>
+      <li>Team</li>
+
+    </ul>
+
+  </div>
+  <div className="col-lg-3 mt-3">
+    <ul style={{listStyle:"none"}}>
+
+      <li>Education</li>
+      <li>Facilities</li>
+      <li>Coaches</li>
+      <li>Our Academy</li>
+      
+
+    </ul>
+
+  </div>
+  <div className="col-lg-3 mt-3">
+    <ul style={{listStyle:"none"}}>
+
+      <li>U13 team</li>
+      <li>U15 team</li>
+      <li>Philosophy</li>
+      <li>Programmes</li>
+     
+
+    </ul>
+
+  </div>
+
+</div>
+
+ 
+
     </div>
+    
+    </>
   );
 }
 
